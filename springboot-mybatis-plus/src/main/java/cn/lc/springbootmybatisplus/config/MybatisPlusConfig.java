b33749b2c9ff85749ebc2bd0df908df7de509572
liucong <1185829245@qq.com>
2021-01-06 16:07:57 +0800

    分页插件

---
+++ springboot-mybatis-plus/src/main/java/cn/lc/springbootmybatisplus/config/MybatisPlusConfig.java
@@ -0,0 +1,28 @@
+package cn.lc.springbootmybatisplus.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Author lc
+ * @Date 2021/1/4/004 15:28
+ * @Description
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor paginationInterceptor() {
+        MybatisPlusInterceptor mybatisPlusInterceptor = new MybatisPlusInterceptor();
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor(DbType.MYSQL);
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        paginationInnerInterceptor.setOverflow(true);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        paginationInnerInterceptor.setMaxLimit(100L);
+        mybatisPlusInterceptor.addInnerInterceptor(paginationInnerInterceptor);
+        return mybatisPlusInterceptor;
+    }
+}
--- springboot-mybatis-plus/src/main/java/cn/lc/springbootmybatisplus/controller/UserController.java
+++ springboot-mybatis-plus/src/main/java/cn/lc/springbootmybatisplus/controller/UserController.java
@@ -2,12 +2,15 @@ package cn.lc.springbootmybatisplus.controller;
 
 import cn.lc.springbootmybatisplus.bean.User;
 import cn.lc.springbootmybatisplus.mapper.UserMapper;
-import com.baomidou.mybatisplus.extension.activerecord.Model;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.sql.Wrapper;
 
 /**
  * @Author lc
@@ -21,19 +24,41 @@ public class UserController {
     private UserMapper userMapper;
 
     @GetMapping
-    public User getUserById(@Param("id") String id){
+    public User getUserById(@Param("id") String id) {
         return userMapper.selectById(id);
     }
 
     /**
      * AR查询操作   AR模式提供了一种更便捷的方式实现CRUD操作，其本质还是调用Mybatis的方法
+     *
      * @param id
      * @return
      */
     @GetMapping(value = "/ar")
-    public User getUserByIdAR(@Param("id") String id){
+    public User getUserByIdAR(@Param("id") String id) {
         User user = new User();
         User select = user.selectById(id);
         return select;
+    }
+
+
+    @GetMapping(value = "/page")
+    public Page<User> page() {
+        Page<User> page = new Page<>(1, 5);
+
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.orderByDesc("id");
+
+        queryWrapper.like("name","t").or();
+        queryWrapper.like("email","t");
+
+        Page<User> userPage = userMapper.selectPage(page, queryWrapper);
+        System.out.println("总页码："+page.getTotal());
+        System.out.println("当前页码："+page.getCurrent());
+        System.out.println("总页码："+page.getPages());
+        System.out.println("每页显示的条数："+page.getSize());
+        System.out.println("是否有上一页："+page.hasPrevious());
+        System.out.println("是否有下一页："+page.hasNext());
+        return userPage;
     }
 }
